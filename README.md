4e330dd96089a43c10f04e0189c10cc2461aa0cc
felixrevol <f-reinhardt@live.com>
2020-05-13 10:39:44 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# vuetify-nuxt-generate-expansion-panel-issue
+
+>
+
+## Steps to reproduce
+
+```bash
+# install dependencies
+$ npm install
+
+# generate static project
+$ npm run generate
+
+# open dist/index.html => expansion-panel CSS is missing
+
+# working properly if not generated:
+$ npm run dev
+```
+
+## result
+
+![Alt text](./result.png?raw=true "dist/index.html")
+
+## expected
+
+![Alt text](./expected.png?raw=true "run dev")
---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-layout>
+    <v-flex class="text-center">
+      <v-expansion-panels>
+        <v-expansion-panel>
+          <v-expansion-panel-header>Heading 1</v-expansion-panel-header>
+          <v-expansion-panel-content
+            >Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
+            libero repellendus nulla possimus corporis officia omnis! Animi
+            eaque numquam impedit asperiores veniam cum quibusdam labore
+            reprehenderit ipsam, sapiente sit!
+            Dignissimos.</v-expansion-panel-content
+          >
+        </v-expansion-panel>
+        <v-expansion-panel>
+          <v-expansion-panel-header>Heading 2</v-expansion-panel-header>
+          <v-expansion-panel-content
+            >Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
+            similique, voluptatum labore ipsam excepturi sint eaque cum minus
+            aut esse placeat ullam fugiat dolor? Repellendus dolorum sapiente
+            non nulla sunt.</v-expansion-panel-content
+          >
+        </v-expansion-panel>
+      </v-expansion-panels>
+    </v-flex>
+  </v-layout>
+</template>
